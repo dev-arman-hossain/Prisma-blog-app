38de26221004b805ebf7cd99905debb2d85cc4ff
Arman Hossain <web.devarman@gmail.com>
2025-12-29 05:42:25 +0600

    add user authentication middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,58 @@
+import type { NextFunction, Request, Response } from "express";
+import { auth as betterAuth } from "../lib/auth";
+
+export enum UserRole {
+  ADMIN = "admin",
+  USER = "user",
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: string;
+        name: string;
+        emailVerified: boolean;
+      };
+    }
+  }
+}
+
+const auth = (...roles: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get user session
+      const session = await betterAuth.api.getSession({
+        headers: req.headers as any,
+      });
+
+      if (!session) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      if (!session.user.emailVerified) {
+        return res
+          .status(403)
+          .json({ message: "Please verify your email to access" });
+      }
+
+      req.user = {
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified,
+      };
+      if (roles.length && !roles.includes(req.user.role as UserRole)) {
+        return res.status(403).json({ message: "Forbidden" });
+      }
+      next();
+    } catch (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  };
+};
+
+export default auth;
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -2,20 +2,25 @@ import type { Request, Response } from "express";
 import type { post } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async (data: Omit<post, "id" | "createdAt" | "updatedAt">) => {
-    // Logic to create a new post
-    const result = await prisma.post.create({
-        data
-    })
-    return result;
-}
+const createPost = async (
+  data: Omit<post, "id" | "createdAt" | "updatedAt" | "authorId">,
+  userId: string
+) => {
+  const result = await prisma.post.create({
+    data: {
+      ...data,
+      authorId: userId,
+    },
+  });
+  return result;
+};
 
 const getAllPosts = async (req: Request, res: Response) => {
-    const result = await prisma.post.findMany();
-    return result;
-}
+  const result = await prisma.post.findMany();
+  return result;
+};
 
 export const PostService = {
-    createPost,
-    getAllPosts,
+  createPost,
+  getAllPosts,
 };
