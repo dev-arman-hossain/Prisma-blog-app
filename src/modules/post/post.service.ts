2c04c1404ba1c052818031e23f9494bb998bacf4
Arman Hossain <web.devarman@gmail.com>
2026-01-03 05:26:08 +0600

    enhance post retrieval to include tag search functionality

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -31,6 +31,11 @@ const getAllPosts = async (payload: { search: string | undefined }) => {
             mode: "insensitive",
           },
         },
+        {
+          tags: {
+            has: payload.search as string,
+          },
+        },
       ],
     },
   });
